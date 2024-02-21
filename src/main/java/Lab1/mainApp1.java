e50177db4f9ba49ef6b25b3b6a246c0ec0c32eb8
mindy <xxieal@ust.hk>
2024-02-21 16:13:52 +0800

    This is an initial commit for Comp3111LEx Project

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -4,10 +4,12 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("Hello!");
         System.out.println("Welcome to Scientific Calculator!");
         System.out.println("Program started ...");
         System.out.println(b + " to power " + n + " = " + myLibrary.Power(b, n));
         System.out.println(n + " ! = " + myLibrary.factorial(n));
         System.out.println("Program ... Ended ...");
+        System.out.println("Goodbye!");
     }
 }
